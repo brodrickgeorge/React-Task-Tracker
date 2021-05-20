2f9591630b6bbcb9f59df4d26789d8fc88ce38ab
brodrickgeorge <brodrick.george@yahoo.com>
2021-05-20 11:54:50 -0500

    delete task

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import Button from "./Button";
 import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" />
-      </header>
-      
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
   );
 };
 
